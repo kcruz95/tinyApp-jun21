7191d2ad73a7244703f8b2833395c1e44276d55b
kcruz95 <kyle.cruz@hotmail.ca>
2021-07-05 16:56:25 -0700

    starting tinyApp

---
+++ app.js
@@ -0,0 +1,27 @@
+// Review and experiment with the following simple web server code.
+
+// It consists of a basic HTTP web server that responds with a string containing the request path and method.
+
+const { request, response } = require("express");
+const http = require("http");
+const PORT = 8080;
+
+// a function which handles requests and sends response
+const requestHandler = function(request, response) {
+  if(request.url === "/")
+  response.end("Welcome!");
+};
+
+if (request.url === "urls") {
+  response.end("www.lighthouselabs.ca\nwww.google.com")
+};
+
+if (response.statusCode = 404) {
+  response.end("404 Page Not Found")
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(PORT, () => {
+  console.log(`Server listening on: http://localhost:${PORT}`);
+});
