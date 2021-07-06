7a00f42aec12a70a736a14bc0f3b753e7d1e4561
kcruz95 <kyle.cruz@hotmail.ca>
2021-07-06 16:38:38 -0700

    URL shortener pt2

--- express_server.js
+++ express_server.js
@@ -1,13 +1,30 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+  // let result = "";
+  // const inputChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  // const charLength = inputChars.length;
+  // const length = 8;
+  // for (i = 0; i < length; i = i + 1) {
+  //   result += inputChars.charAt(Math.floor(Math.random() * charLength));
+  // }
+  // return result;
+}
+
+// GET POST fxns
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -21,6 +38,10 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
@@ -29,4 +50,9 @@ app.get("/urls", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: longURL };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
