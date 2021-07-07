e68b882a32af5b8618d5fe1475b064e18493068c
kcruz95 <kyle.cruz@hotmail.ca>
2021-07-06 17:58:15 -0700

    URL shortening pt 2 done i think?

--- express_server.js
+++ express_server.js
@@ -9,8 +9,10 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": {longURL: "http://www.lighthouselabs.ca", user_id: "dm3"},
+  "9sm5xK": {longURL: "http://www.google.com", user_id: "dm3"},
+  "b6UTxQ": {longURL: "https://www.tsn.ca", user_id: "dm3"},
+  "i3BoGr": {longURL: "https://www.google.ca", user_id: "dm3"}
 };
 
 function generateRandomString() {
@@ -48,11 +50,27 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: longURL };
+  const longURL = urlDatabase[req.params.shortURL].longURL;
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: longURL,
+    user_id: req.cookies.user_id
+  };
+  console.log("104", longURL);
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL].longURL;
+  res.redirect(longURL);
+});
+
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  console.log(req.body); // Log the POST request body to the console
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = {
+    longURL: req.body.longURL,
+    user_id: req.cookies.user_id,
+  }
+  res.redirect(`urls/${shortURL}`)
 });
